9e9e758bcfab72920565811ef20a939d21e04407
Yugandhar Prasad <yugandhar1224@gmail.com>
2021-08-28 23:15:51 +0100

    add test id's and prettier

--- README.md
+++ README.md
@@ -1,30 +1,31 @@
-# Hopin's Test and Automation Challenge #
+# Hopin's Test and Automation Challenge
 
 ## Tasks:
+
 - [x] Test Environment Setup
 - [x] Create a test plan and run it mannually
 - [ ] Create an automated API level test scenario
 - [ ] Create an automated UI level test scenario
 
 ## Test Plan:
-| User screen    | Test case                                         | Pass/Fail          |
-| :---           |                 :---:                             |       :---:        |
-| Welcome Screen | User presented with a welcome message             | :white_check_mark: | 
-| Welcome Screen | User should see a text field to enter his/her name| :white_check_mark: |
-| Welcome Screen | User should be able to press submit button        | :white_check_mark: |
-| Welcome Screen | I should see an alert if I press submit without entering my name |:white_check_mark: |
-| Welcome Screen | User should not able to enter numbers or special characters in name field| :x: |
-| Welcome Screen | User should not able to enter emoji markup in name field| :x: |
-| Customers Screen | User presented with his/her name and current data | :white_check_mark: |
-| Customers Screen | Customer list will be displayed in table format  | :white_check_mark: |
-| Customers Screen | Customer details will be displayed if I click on the company name  | :white_check_mark: |
-| Customers Screen | Only company name has to have provision to click  | :white_check_mark: |
-| Customers Screen | Customer comapny size is displayed based on the no.of employees   | :white_check_mark: |
-| Customers Screen | I should be able to navigate into all company details pages   | :x: |
-| Customers Screen | Size of the company should say *Small* if no.of employees is less than or equal 100   | :white_check_mark: |
-| Customers Screen | Size of the company should say *Medium* if no.of employees greater then 10 and less then or equal 1000   | :white_check_mark: |
-| Customers Screen | Size of the company should say *Big* if no.of employees greater then 1000| :x: |
-| Contacts Detail Screen | I should see company details like Name, no.of employees, size and contact information    | :white_check_mark: |
-| Contacts Detail Screen | I should see a message like *no contact information available* if contact details are not available| :x: |
-| Contacts Detail Screen | I should see and click on *Back to the list* button | :white_check_mark: |
 
+| User screen            | Test case                                                                                              |     Pass/Fail      |
+| :--------------------- | :----------------------------------------------------------------------------------------------------- | :----------------: |
+| Welcome Screen         | User presented with a welcome message                                                                  | :white_check_mark: |
+| Welcome Screen         | User should see a text field to enter his/her name                                                     | :white_check_mark: |
+| Welcome Screen         | User should be able to press submit button                                                             | :white_check_mark: |
+| Welcome Screen         | I should see an alert if I press submit without entering my name                                       | :white_check_mark: |
+| Welcome Screen         | User should not able to enter numbers or special characters in name field                              |        :x:         |
+| Welcome Screen         | User should not able to enter emoji markup in name field                                               |        :x:         |
+| Customers Screen       | User presented with his/her name and current data                                                      | :white_check_mark: |
+| Customers Screen       | Customer list will be displayed in table format                                                        | :white_check_mark: |
+| Customers Screen       | Customer details will be displayed if I click on the company name                                      | :white_check_mark: |
+| Customers Screen       | Only company name has to have provision to click                                                       | :white_check_mark: |
+| Customers Screen       | Customer comapny size is displayed based on the no.of employees                                        | :white_check_mark: |
+| Customers Screen       | I should be able to navigate into all company details pages                                            |        :x:         |
+| Customers Screen       | Size of the company should say _Small_ if no.of employees is less than or equal 100                    | :white_check_mark: |
+| Customers Screen       | Size of the company should say _Medium_ if no.of employees greater then 10 and less then or equal 1000 | :white_check_mark: |
+| Customers Screen       | Size of the company should say _Big_ if no.of employees greater then 1000                              |        :x:         |
+| Contacts Detail Screen | I should see company details like Name, no.of employees, size and contact information                  | :white_check_mark: |
+| Contacts Detail Screen | I should see a message like _no contact information available_ if contact details are not available    |        :x:         |
+| Contacts Detail Screen | I should see and click on _Back to the list_ button                                                    | :white_check_mark: |
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,102 +1,137 @@
-  import React, {Component} from 'react';
+import React, { Component } from "react";
 
-const serverPort = 3001; 
+const serverPort = 3001;
 const serverURL = `http://localhost:${serverPort}/`;
 
 class CustomerApp extends Component {
-
   state = {
     name: null,
     timestamp: null,
     customers: null,
-    customer: null
+    customer: null,
   };
 
-  async getCustomer(customer){
-    this.setState({customer})
+  async getCustomer(customer) {
+    this.setState({ customer });
   }
 
-  async getCustomers(){
+  async getCustomers() {
     const userName = document.getElementById("name").value;
-    if(!userName || userName === ""){
+    if (!userName || userName === "") {
       alert("Please provide your name");
       return;
     }
 
-    const axios = require('axios');
+    const axios = require("axios");
     const server = axios.create({
-      baseURL: serverURL
+      baseURL: serverURL,
     });
 
     try {
-      const response = await server.post('/', { name: userName });
+      const response = await server.post("/", { name: userName });
       const { name, timestamp, customers } = response.data;
-      this.setState({name, timestamp, customers });
+      this.setState({ name, timestamp, customers });
     } catch (error) {
       alert(error);
     }
   }
 
+  getContactText = () => {
+    return this.state.customer.contactInfo
+      ? `${this.state.customer.contactInfo.name} (${this.state.customer.contactInfo.email})`
+      : "No contact info available";
+  };
+
   render() {
     return (
       <div>
-        { !this.state.name &&
+        {!this.state.name && (
           <div>
             <p>Please provide your name:</p>
             <p>
-              <input type="text" id="name"/>
-              <input type="button" value="Submit" onClick={this.getCustomers.bind(this)}/>
+              <input type="text" id="name" />
+              <input
+                type="button"
+                value="Submit"
+                id="form-submit"
+                onClick={this.getCustomers.bind(this)}
+              />
             </p>
           </div>
-        }
-        { this.state.name && 
+        )}
+        {this.state.name && (
           <div>
-            <p>Hi <b>{this.state.name}</b>. It is now <b>{this.state.timestamp}</b> and here is our customer list. Click on each of them to view their contact details.</p>
-            { !this.state.customer &&
-            <div>
-              <table border="1">
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th># of Employees</th>
-                    <th>Size</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  { this.state.customers.map(customer => 
-                    <tr key={customer.id}>
-                      <td><a href="#" onClick={() => this.getCustomer(customer)}>{customer.name}</a></td>
-                      <td>{customer.employees}</td>
-                      <td>{customer.size}</td>
-                    </tr>  
-                  )}
-                </tbody>
-              </table>
-            </div>
-            }
-            { this.state.customer &&
+            <p id="greeting">
+              Hi <b>{this.state.name}</b>. It is now{" "}
+              <b>{this.state.timestamp}</b> and here is our customer list. Click
+              on each of them to view their contact details.
+            </p>
+            {!this.state.customer && (
               <div>
-                <p><b>Customer Details</b></p>
-                <p><b>Name:</b> {this.state.customer.name}</p>
-                <p><b># of Employees:</b> {this.state.customer.employees}</p>
-                <p><b>Size:</b> {this.state.customer.size}</p>
-                <p><b>Contact:</b> {this.state.customer.contactInfo.name} ({this.state.customer.contactInfo.email})</p>
-                <input type="button" value="Back to the list" onClick={() => this.setState({customer: null})}/>
+                <table id="customers-list" border="1">
+                  <thead>
+                    <tr id="customer">
+                      <th id="customer-name-header">Name</th>
+                      <th id="customer-employees-header"># of Employees</th>
+                      <th id="customer-company-size-header">Size</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.state.customers.map((customer) => (
+                      <tr id={`customerInfo`} key={customer.id}>
+                        <td id="customer-name">
+                          <a
+                            href="#"
+                            onClick={() => this.getCustomer(customer)}
+                          >
+                            {customer.name}
+                          </a>
+                        </td>
+                        <td id="customer-employees">{customer.employees}</td>
+                        <td id="customer-company-size">{customer.size}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
               </div>
-            }
+            )}
+            {this.state.customer && (
+              <div id="customer-details">
+                <p id="customer-details-header">
+                  <b>Customer Details</b>
+                </p>
+                <p id="customer-name">
+                  <b>Name:</b> {this.state.customer.name}
+                </p>
+                <p id="customer-noof-Employees">
+                  <b># of Employees:</b> {this.state.customer.employees}
+                </p>
+                <p id="customer-company-size">
+                  <b>Size:</b> {this.state.customer.size}
+                </p>
+                <p id="customer-contact">
+                  <b>Contact:</b> {this.getContactText()}
+                </p>
+                <input
+                  type="button"
+                  id="back-to-customers-list"
+                  value="Back to the list"
+                  onClick={() => this.setState({ customer: null })}
+                />
+              </div>
+            )}
           </div>
-        }
-        
+        )}
       </div>
-    )
+    );
   }
 }
 
 function App() {
   return (
     <div>
-      <h1>Welcome to Customer App</h1>
-      <CustomerApp/>
+      <h1 id="welcome">Welcome to Customer App</h1>
+      <CustomerApp />
     </div>
   );
 }
